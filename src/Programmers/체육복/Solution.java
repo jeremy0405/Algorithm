d731032122d9fd3fffb69825585d0816b4a49373
jeremy0405 <81368630+jeremy0405@users.noreply.github.com>
2021-10-25 22:02:11 +0900

    Create Solution.java

---
+++ src/Programmers/체육복/Solution.java
@@ -0,0 +1,47 @@
+package Programmers.체육복;
+
+import java.util.Arrays;
+
+public class Solution {
+    class Solution {
+        public int solution(int n, int[] lost, int[] reserve) {
+            int answer = 0;
+            answer = n - lost.length;
+
+            Arrays.sort(lost);
+            Arrays.sort(reserve);
+
+            for (int i = 0; i < lost.length; i++) {
+                for (int j = 0; j < reserve.length; j++) {
+
+                    if (lost[i] == reserve[j]) {
+                        answer++;
+                        lost[i] = 50;
+                        reserve[j] = 100;
+                        continue;
+                    }
+                }
+            }
+
+
+            for (int i = 0; i < lost.length; i++) {
+                for (int j = 0; j < reserve.length; j++) {
+                    if (lost[i] - reserve[j] == 1) {
+                        answer++;
+                        lost[i] = 50;
+                        reserve[j] = 100;
+                        continue;
+                    } else if (lost[i] - reserve[j] == -1) {
+                        answer++;
+                        lost[i] = 50;
+                        reserve[j] = 100;
+                        continue;
+                    }
+
+                }
+            }
+
+            return answer;
+        }
+    }
+}
