8646850583d5372bbc0d7546c9fc41ae6f4866b3
Jang HyungSuk <jeremy0405@naver.com>
2021-11-05 20:17:37 +0900

    백준 1157 update case 1개 실패

--- src/Baekjoon/num1157/Main.java
+++ src/Baekjoon/num1157/Main.java
@@ -1,2 +1,64 @@
-package Baekjoon.num1157;public class Main {
+package Baekjoon.num1157;
+
+import java.util.Arrays;
+import java.util.Scanner;
+
+public class Main {
+
+    public static char getChar(char[] c, int n) {
+        char output;
+        if (n == 1) {
+            output = c[0];
+            return output;
+        }
+
+        int[] cnt = new int[26];
+        int j = 0;
+
+        for (int i = 1; i < n; i++) {
+            if (c[i] == c[i - 1]) {
+                cnt[j]++;
+            } else {
+                j++;
+            }
+        }
+
+        int MAX = 0;
+        int idx = 0;
+        for(int i = 0; i < 26; i++){
+            if(MAX < cnt[i]){
+                MAX = cnt[i];
+                idx = i + 1;
+            }
+        }
+
+        for(int i = 0; i < 26; i++){
+            if(MAX == cnt[i]){
+                if(i != idx - 1){
+                    return '?';
+                }
+            }
+        }
+        return c[idx];
+    }
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        String s = sc.nextLine();
+        sc.close();
+
+        String S = s.toUpperCase();
+
+        char[] c = new char[S.length()];
+
+        for (int i = 0; i < S.length(); i++) {
+            c[i] = S.charAt(i);
+        }
+
+        Arrays.sort(c);
+
+        System.out.println(getChar(c, c.length));
+
+    }
+
 }
--- src/Baekjoon/num1157/SecondTry.java
+++ src/Baekjoon/num1157/SecondTry.java
@@ -1,2 +1,28 @@
-package Baekjoon.num1157;public class SecondTry {
+package Baekjoon.num1157;
+
+import java.util.Scanner;
+
+public class SecondTry {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        String str = sc.nextLine().toUpperCase();
+        sc.close();
+
+        int[] cnt = new int[26];
+        int max = 0;
+        char result =' ';
+
+        for (int i = 0; i < str.length(); i++) {
+            cnt[str.charAt(i) - 65]++;
+            if (max < cnt[str.charAt(i) - 65]) {
+                max = cnt[str.charAt(i) - 65];
+                result = str.charAt(i);
+            } else if (max == cnt[str.charAt(i) - 65]) {
+                result = '?';
+            }
+        }
+        System.out.println(result);
+
+    }
+
 }
