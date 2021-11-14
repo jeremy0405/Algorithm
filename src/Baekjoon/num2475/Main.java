7d4eb8908c71f9a21b9979b906ba4b6cbc75ff63
Jang HyungSuk <jeremy0405@naver.com>
2021-11-14 23:10:03 +0900

    백준 2475 풀이

--- src/Baekjoon/num2475/Main.java
+++ src/Baekjoon/num2475/Main.java
@@ -1,2 +1,31 @@
-package Baekjoon.num2475;public class Main {
+package Baekjoon.num2475;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.StringTokenizer;
+
+public class Main {
+
+    public static void main(String[] args) throws IOException {
+
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        List<Integer> nums = new ArrayList<>();
+
+        while (st.hasMoreTokens()) {
+            nums.add(Integer.parseInt(st.nextToken()));
+        }
+
+        int total = 0;
+        for (int i = 0; i < nums.size(); i++) {
+            total += (int) Math.pow(nums.get(i), 2);
+        }
+
+        System.out.println(total % 10);
+
+    }
+
 }
