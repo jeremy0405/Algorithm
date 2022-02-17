76e8ebe73428c3f1b081f7ba0a606d7d484d70a8
hsjang <hsjang0405@gmail.com>
2022-02-17 10:35:10 +0900

    ✨  백준 1654 풀이

--- src/Baekjoon/num1654/Main.java
+++ src/Baekjoon/num1654/Main.java
@@ -1,18 +1,19 @@
 package Baekjoon.num1654;
 
 /*
-	문제    : BOJ 덩치
-    유형    : 구현, 브루트포스
-	난이도   : HARD (풀고나니 Medium ~ EASY) (S5)
-	시간    : 120m
-	uri    : https://www.acmicpc.net/problem/7568
-    날짜    : 22.02.16(x)
-    refer  :
+	문제    : BOJ 랜선 자르기
+    유형    : 이분탐색
+	난이도   : HARD (S3)
+	시간    : 100m
+	uri    : https://www.acmicpc.net/problem/1654
+    날짜    : 22.02.17(o)
+    refer  : https://dundung.tistory.com/53
 */
 
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
+import java.util.Arrays;
 
 public class Main {
 
@@ -23,33 +24,35 @@ public class Main {
 		int n = Integer.parseInt(input[1]);
 
 		int[] lan = new int[k];
-		int total = 0;
 
 		for (int i = 0; i < k; i++) {
-			int tmp = Integer.parseInt(br.readLine());
-			lan[i] = tmp;
-			total += tmp;
+			lan[i] = Integer.parseInt(br.readLine());
 		}
 
-		int maxLength = total / n;
-		final int MAX_LENGTH = maxLength;
-		int num = 0;
-		while (num != n) {
-			num = 0;
+		Arrays.sort(lan);
 
+		long max = lan[k - 1];
+		long min = 1;
+		long middle = 0;
+		long answer = 0;
+
+		while (max >= min) {
+			middle = (max + min) / 2;
+			long count = 0;
 			for (int i = 0; i < k; i++) {
-				num += lan[i] / maxLength;
+				count += lan[i] / middle;
 			}
 
-			if (num > n) {
-				maxLength = (MAX_LENGTH + maxLength) / 2 - 1;
-			} else if (num < n) {
-				maxLength = maxLength / 2 + 1;
+			if (count >= n) {
+				answer = middle;
+				min = middle + 1;
+			} else {
+				max = middle - 1;
 			}
 
 		}
 
-		System.out.println(maxLength);
+		System.out.println(answer);
 
 	}
 
