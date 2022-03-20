73caf1dfa7e9f07c3a59472358d8f9dcb4f979dc
hsjang <hsjang0405@gmail.com>
2022-03-20 14:45:49 +0900

    ✨  백준 5525 풀이

--- src/Baekjoon/num5525/Main.java
+++ src/Baekjoon/num5525/Main.java
@@ -3,17 +3,16 @@ package Baekjoon.num5525;
 /*
 	문제    : BOJ IOIOI
     유형    :
-	난이도   : EASY(S2)
-	시간    : m
+	난이도   : MEDIUM(S2)
+	시간    : 60m
 	uri    : https://www.acmicpc.net/problem/5525
-    날짜    : 22.03.19(o)
+    날짜    : 22.03.20(o)
     refer  :
 */
 
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.util.Arrays;
 
 public class Main {
 
@@ -21,24 +20,18 @@ public class Main {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 		int n = Integer.parseInt(br.readLine());
 		int m = Integer.parseInt(br.readLine());
-		char[] arr = new char[m];
+		char[] arr = br.readLine().toCharArray();
 
-		char[] p = new char[2 * n + 1];
-		for (int i = 0; i < 2 * n; i += 2) {
-			p[i] = 'I';
-			p[i + 1] = 'O';
-		}
-		p[2 * n] = 'I';
-
-		for (int i = 0; i < m; i++) {
-			arr[i] = (char) br.read();
-		}
+		int[] repeat = new int[m];
 
 		int count = 0;
-		for (int i = 0; i < m - (2 * n + 1); i++) {
-			char[] chars = Arrays.copyOfRange(arr, i, i + 2 * n + 1);
-			if (Arrays.compare(chars, p) == 0) {
-				count++;
+
+		for (int i = 1; i < m - 1; i++) {
+			if (arr[i] == 'O' && arr[i + 1] == 'I') {
+				repeat[i + 1] = repeat[i - 1] + 1;
+				if (repeat[i + 1] >= n && arr[i - 2 * n + 1] == 'I') {
+					count++;
+				}
 			}
 		}
 		System.out.println(count);
