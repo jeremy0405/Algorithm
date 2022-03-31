b68227d252220e6189f9875fdd2309ead185ce56
hsjang <hsjang0405@gmail.com>
2022-03-31 21:21:49 +0900

    ✨  백준 15650 N과 M (2) 풀이

---
+++ src/Baekjoon/num15650/Main.java
@@ -0,0 +1,57 @@
+package Baekjoon.num15650;
+
+/*
+	문제    : BOJ N과 M (2)
+    유형    : 백트래킹
+	난이도   : MEDIUM(S3)
+	시간    : 30m
+	uri    : https://www.acmicpc.net/problem/15650
+    날짜    : 22.03.31(o)
+    refer  :
+*/
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class Main {
+
+	public static int[] arr;
+	public static int N, M;
+	public static StringBuilder sb = new StringBuilder();
+
+	public static void main(String[] args) throws IOException {
+
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		StringTokenizer st = new StringTokenizer(br.readLine());
+
+		N = Integer.parseInt(st.nextToken());
+		M = Integer.parseInt(st.nextToken());
+
+		arr = new int[M];
+
+		dfs(1, 0);
+		System.out.println(sb);
+
+	}
+
+	public static void dfs(int at, int depth) {
+
+		if (depth == M) {
+			for (int val : arr) {
+				sb.append(val).append(' ');
+			}
+			sb.append('\n');
+			return;
+		}
+
+		for (int i = at; i <= N; i++) {
+
+			arr[depth] = i;
+			dfs(i + 1, depth + 1);
+
+		}
+	}
+
+}
