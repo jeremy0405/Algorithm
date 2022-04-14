7903d8c642b0b743da182c1ef725ed1a6258afa2
hsjang <hsjang0405@gmail.com>
2022-04-14 12:37:18 +0900

    ✨  백준 1764 듣보잡 풀이

--- src/Baekjoon/num1764/Main.java
+++ src/Baekjoon/num1764/Main.java
@@ -15,7 +15,10 @@ import java.io.IOException;
 import java.io.InputStreamReader;
 import java.util.ArrayList;
 import java.util.Collections;
+import java.util.HashSet;
 import java.util.List;
+import java.util.Set;
+import java.util.Stack;
 
 public class Main {
 
@@ -25,7 +28,7 @@ public class Main {
 		int n = Integer.parseInt(s[0]);
 		int m = Integer.parseInt(s[1]);
 
-		List<String> store = new ArrayList<>();
+		Set<String> store = new HashSet<>();
 
 		for (int i = 0; i < n; i++) {
 			store.add(br.readLine());
@@ -35,17 +38,22 @@ public class Main {
 
 		for (int i = 0; i < m; i++) {
 			String name = br.readLine();
-			if (store.contains(name)) {
+			if (!store.add(name)) {
 				answer.add(name);
 			}
 		}
 
 		Collections.sort(answer);
 
-		System.out.println(answer.size());
+		StringBuilder sb = new StringBuilder();
+
+		sb.append(answer.size()).append('\n');
+
 		for (String name : answer) {
-			System.out.println(name);
+			sb.append(name).append('\n');
 		}
+
+		System.out.println(sb);
 
 	}
 
