31db04125e7bb7246b55b9905fcdb09222c9fc36
hsjang <hsjang0405@gmail.com>
2022-04-23 22:25:29 +0900

    ✨  백준 11653 소인수분해 풀이

---
+++ src/Baekjoon/num11653/Main.java
@@ -0,0 +1,35 @@
+package Baekjoon.num11653;
+
+/*
+	문제    : BOJ 소인수분해
+    유형    :
+	난이도   : EASY(S5)
+	시간    : 10m
+	uri    : https://www.acmicpc.net/problem/11653
+    날짜    : 22.04.23(o)
+    refer  :
+*/
+
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.io.IOException;
+
+public class Main {
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+		StringBuilder sb = new StringBuilder();
+		int N = Integer.parseInt(br.readLine());
+
+		for (int i = 2; i <= Math.sqrt(N); i++) {
+			while (N % i == 0) {
+				sb.append(i).append('\n');
+				N /= i;
+			}
+		}
+		if (N != 1) {
+			sb.append(N);
+		}
+		System.out.println(sb);
+	}
+}
