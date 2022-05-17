2eb2aca68c31519f6c98ddb1438f7631a016bec7
hsjang <hsjang0405@gmail.com>
2022-05-17 21:58:04 +0900

    ✨  백준 1012 유기농 배추 풀이
    bfs로 풀 때는 offer이후에 visited를 갱신해야 함!!!!

--- src/Baekjoon/num1012/Main.java
+++ src/Baekjoon/num1012/Main.java
@@ -2,9 +2,9 @@ package Baekjoon.num1012;
 
 /*
 	문제    : BOJ 유기농 배추
-    유형    :
-	난이도   :
-	시간    : m
+    유형    : bfs, dfs
+	난이도   : MEDIUM(S2)
+	시간    : 40m
 	uri    : https://www.acmicpc.net/problem/1012
     날짜    : 22.05.17(o)
     refer  :
@@ -20,6 +20,10 @@ public class Main {
 
 	static int[] dx = {1, -1, 0, 0};
 	static int[] dy = {0, 0, 1, -1};
+	static int col;
+	static int row;
+	static boolean[][] visited;
+	static boolean[][] map;
 
 
 	public static void main(String[] args) throws IOException {
@@ -29,12 +33,12 @@ public class Main {
 		while (repeat-- > 0) {
 
 			String[] split = br.readLine().split(" ");
-			int col = Integer.parseInt(split[0]);
-			int row = Integer.parseInt(split[1]);
+			col = Integer.parseInt(split[0]);
+			row = Integer.parseInt(split[1]);
 			int number = Integer.parseInt(split[2]);
 
-			boolean[][] visited = new boolean[row][col];
-			boolean[][] map = new boolean[row][col];
+			visited = new boolean[row][col];
+			map = new boolean[row][col];
 
 			for (int i = 0; i < number; i++) {
 				String[] s = br.readLine().split(" ");
@@ -45,7 +49,7 @@ public class Main {
 			for (int i = 0; i < row; i++) {
 				for (int j = 0; j < col; j++) {
 					if (map[i][j] && !visited[i][j]) {
-						bfs(col, row, visited, map, i, j);
+						bfs(i, j);
 						count++;
 					}
 				}
@@ -54,13 +58,15 @@ public class Main {
 		}
 	}
 
-	private static void bfs(int col, int row, boolean[][] visited, boolean[][] map, int i,
-		int j) {
+	private static void bfs(int i, int j) {
 		Queue<int[]> q = new LinkedList<>();
-		q.add(new int[]{i, j});
+
+		q.offer(new int[]{i, j});
+		visited[i][j] = true;
+
 		while (!q.isEmpty()) {
 			int[] rowCol = q.poll();
-			visited[rowCol[0]][rowCol[1]] = true;
+
 			for (int k = 0; k < 4; k++) {
 				int newRow = rowCol[0] + dx[k];
 				int newCol = rowCol[1] + dy[k];
@@ -71,6 +77,7 @@ public class Main {
 
 				if (map[newRow][newCol] && !visited[newRow][newCol]) {
 					q.offer(new int[]{newRow, newCol});
+					visited[newRow][newCol] = true;
 				}
 
 			}
