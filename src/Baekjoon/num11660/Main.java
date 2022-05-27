473a9914543cff720c99b0a6ff72134d7017c422
hsjang <hsjang0405@gmail.com>
2022-05-28 00:38:20 +0900

    ✨  백준 11660 구간 합 구하기 5 풀이

--- src/Baekjoon/num11660/Main.java
+++ src/Baekjoon/num11660/Main.java
@@ -2,9 +2,9 @@ package Baekjoon.num11660;
 
 /*
 	문제    : 구간 합 구하기 5
-    유형    : 구현
-	난이도   : S1
-	시간    : m
+    유형    : dp
+	난이도   : MEDIUM(S1)
+	시간    : 20m
 	uri    : https://www.acmicpc.net/problem/11660
     날짜    : 22.05.28(o)
     refer  :
@@ -25,11 +25,16 @@ public class Main {
 		int m = Integer.parseInt(st.nextToken());
 
 		int[][] arr = new int[n + 1][n + 1];
+		int[][] dp = new int[n + 1][n + 1];
 
 		for (int i = 1; i <= n; i++) {
 			st = new StringTokenizer(br.readLine());
+			int sum = 0;
 			for (int j = 1; j <= n; j++) {
-				arr[i][j] = Integer.parseInt(st.nextToken());
+				int tmp = Integer.parseInt(st.nextToken());
+				arr[i][j] = tmp;
+				sum += tmp;
+				dp[i][j] = dp[i - 1][j] + sum;
 			}
 		}
 
@@ -39,22 +44,8 @@ public class Main {
 			int y1 = Integer.parseInt(st.nextToken());
 			int x2 = Integer.parseInt(st.nextToken());
 			int y2 = Integer.parseInt(st.nextToken());
-			printAnswer(x1, y1, x2, y2, arr);
+			System.out.println(dp[x2][y2] + dp[x1 - 1][y1 - 1] - dp[x1 - 1][y2] - dp[x2][y1 - 1]);
 		}
-
-	}
-
-	private static void printAnswer(int x1, int y1, int x2, int y2, int[][] arr) {
-
-		int answer = 0;
-
-		for (int i = x1; i <= x2; i++) {
-			for (int j = y1; j <= y2; j++) {
-				answer += arr[i][j];
-			}
-		}
-
-		System.out.println(answer);
 
 	}
 
