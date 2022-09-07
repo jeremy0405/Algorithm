3238c478c58e88964614b809b85b5586f3485b96
hsjang <hsjang0405@gmail.com>
2022-09-07 22:26:14 +0900

    ✨  프로그래머스 [1차] 뉴스 클러스터링 풀이

---
+++ src/Programmers/일차뉴스클러스터링/Solution.java
@@ -0,0 +1,52 @@
+package Programmers.일차뉴스클러스터링;
+
+/*
+	문제    : 프로그래머스 [1차] 뉴스 클러스터링
+    유형    : 구현
+	난이도   : MEDIUM (level2)
+	시간    : 30m
+	uri    : https://school.programmers.co.kr/learn/courses/30/lessons/17677
+    날짜    : 22.09.07(o)
+    refer  :
+*/
+
+import java.util.ArrayList;
+import java.util.List;
+
+class Solution {
+	public int solution(String str1, String str2) {
+		List<String> listA = createList(str1);
+		List<String> listB = createList(str2);
+
+		int intersection = 0;
+
+		for (String s : listA) {
+			if (listB.contains(s)) {
+				intersection++;
+				listB.remove(s);
+			}
+		}
+
+		int sum = listA.size() + listB.size();
+
+		if (sum == 0) {
+			return 65536;
+		}
+
+		return (int) Math.floor(((double) intersection / sum) * 65536);
+	}
+
+	private List<String> createList(String input) {
+
+		List<String> list = new ArrayList<>();
+
+		for (int i = 0; i < input.length() - 1; i++) {
+			String sub = input.substring(i, i + 2);
+
+			if (sub.chars().allMatch(Character::isLetter)) {
+				list.add(sub.toLowerCase());
+			}
+		}
+		return list;
+	}
+}
