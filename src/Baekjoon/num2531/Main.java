2975c357eb4ae640d20e5f244bf848a6b184eb70
hsjang <hsjang0405@gmail.com>
2022-10-02 21:12:41 +0900

    ✨  백준 2531 회전초밥 풀이

---
+++ src/Baekjoon/num2531/Main.java
@@ -0,0 +1,68 @@
+package Baekjoon.num2531;
+
+/*
+	문제    : BOJ 회전 초밥
+    유형    : 슬라이딩 윈도우
+	난이도   : MEDIUM(S1)
+	시간    : 40m
+	uri    : https://www.acmicpc.net/problem/2531
+    날짜    : 22.10.02(o)
+    refer  :
+*/
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class Main {
+
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		StringTokenizer st = new StringTokenizer(br.readLine());
+		int n = Integer.valueOf(st.nextToken());
+		int d = Integer.valueOf(st.nextToken());
+		int k = Integer.valueOf(st.nextToken());
+		int c = Integer.valueOf(st.nextToken());
+		int[] sushi = new int[n];
+
+		for (int i = 0; i < n; i++) {
+			sushi[i] = Integer.valueOf(br.readLine());
+		}
+
+		int[] ate = new int[d + 1];
+		int count = 0;
+		for (int i = 0; i < k; i++) {
+			if (ate[sushi[i]] == 0) {
+				count++;
+			}
+
+			ate[sushi[i]]++;
+		}
+
+		int maxLen = count;
+
+		for (int i = 1; i < n; i++) {
+			if (maxLen <= count) {
+				if (ate[c] == 0) {
+					maxLen = count + 1;
+				} else {
+					maxLen = count;
+				}
+			}
+
+			int end = (i + k - 1) % n;
+			if (ate[sushi[end]] == 0) {
+				count++;
+			}
+			ate[sushi[end]]++;
+
+			ate[sushi[i - 1]]--;
+			if (ate[sushi[i - 1]] == 0) {
+				count--;
+			}
+		}
+
+		System.out.println(maxLen);
+	}
+}
